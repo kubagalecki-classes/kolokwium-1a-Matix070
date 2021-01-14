80f4aab8528828f056e61d7e0861b248bf3df29b
Matix070 <56588768+Matix070@users.noreply.github.com>
2021-01-14 12:54:37 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -5,4 +5,28 @@
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
 // Zad1
-// tutaj definicja klasy Tagliatelle
+ class Tagliatelle : public Makaron
+
+{
+public:
+
+	Tagliatelle(const double L, const double W, const double R) 
+		: L(L), W(W), R(R)
+	{
+	}
+	Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+
+	double ileMaki(unsigned P) const
+	{
+		return P * L * W * (1. - R) * C;
+	}
+
+private:
+
+	double L;
+	double W;
+	double R;
+	static const double C;
+
+
+};
