b664a63c0163625f16bd36e1720503b7408a5837
Matix070 <56588768+Matix070@users.noreply.github.com>
2021-01-14 13:09:18 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,6 +4,22 @@
 #include <string>
 #include <type_traits>
 
-// tutaj klasa Penne
+ class Penne : public Makaron
+{
+	double ileMaki(unsigned P) const
+	{
+		return P;
+	}
+};
 
-// tutaj definicja metody gotujMakaron
+Makaron* Makaron::gotujMakaron(const std::string& s)
+{
+	if (s.front() == s.back())
+	{
+		return new Tagliatelle(3.14, 0.42, 0.1);
+	}
+	else
+	{
+		return new Penne();
+	}
+}
